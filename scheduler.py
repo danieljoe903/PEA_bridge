baf8b6a0b77d7298e6786fc3d637988b821e9f62
daniel <Danieljoe903@gmail.com>
2026-05-16 03:51:33 +0100

    added changes in scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,14 +1,17 @@
 import schedule
 import time
-from pkg import create_app
-from pkg.task import check_expired_properties
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv("/home/danieljoe903/PEA_bridge/.env")
+
+from pkg import create_app
+from pkg.task import check_expired_properties
 
 app = create_app()
 
-schedule.every(5).minutes.do(check_expired_properties,app)
+schedule.every(5).minutes.do(check_expired_properties, app)
+
+print("Scheduler started...")
 
 while True:
     schedule.run_pending()
